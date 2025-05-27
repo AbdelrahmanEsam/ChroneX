ec6332dd7b92274463e2497c462b14b548c026a6
abdelrahmanesam20000 <abdelrahmanesam20000@gmail.com>
2025-05-27 05:04:06 +0300

    Initial commit

---
+++ androidApp/build.gradle.kts
@@ -0,0 +1,46 @@
+plugins {
+    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.kotlinAndroid)
+    alias(libs.plugins.compose.compiler)
+}
+
+android {
+    namespace = "com.apptikar.chroneX"
+    compileSdk = libs.versions.compileSdk.get().toInt()
+    defaultConfig {
+        applicationId = "com.apptikar.chroneX"
+        minSdk = libs.versions.minSdk.get().toInt()
+        targetSdk = libs.versions.targetSdk.get().toInt()
+        versionCode = 1
+        versionName = "1.0"
+    }
+    buildFeatures {
+        compose = true
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
+    }
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_21.toString()
+    }
+}
+
+dependencies {
+    implementation(projects.shared)
+    implementation(libs.compose.ui)
+    implementation(libs.compose.ui.tooling.preview)
+    implementation(libs.compose.material3)
+    implementation(libs.androidx.activity.compose)
+    debugImplementation(libs.compose.ui.tooling)
+}
---
+++ shared/src/commonMain/kotlin/com/apptikar/chroneX/Platform.kt
@@ -0,0 +1,7 @@
+package com.apptikar.chroneX
+
+interface Platform {
+    val name: String
+}
+
+expect fun getPlatform(): Platform
